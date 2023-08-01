78f0762b80631f35c144836d1d8b46357fd18bd1
Muhammad Maaz Faisal <m.maazfaisal0301@gmail.com>
2023-08-01 19:02:50 +0500

    Update system_db.sql

--- system_db.sql
+++ system_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2023 at 06:29 PM
+-- Generation Time: Aug 01, 2023 at 04:02 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
